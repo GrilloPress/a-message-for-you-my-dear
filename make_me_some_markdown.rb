f81e5d6a0b55558699ef1ec312fa2965d0557f60
GrilloPress <grillopress@gmail.com>
2015-02-21 00:25:03 +0000

    add multi-file method

--- make_me_some_markdown.rb
+++ make_me_some_markdown.rb
@@ -9,14 +9,22 @@ end
 
 def make_me_some_markdown_cleaner
 	puts "write the name of the markdown file"
-	file = gets.chomp.downcase.gsub(/\s+/, “”)
+	file = gets.chomp.downcase.gsub(/\s+/, "")
 	puts `touch #{file}.md`
 end
 
 def make_me_some_markdown_multi_ball
+
 	puts "write the name of the markdown files you want creating"
-	file = gets
+	a = gets.chomp.downcase
+	array = a.split(' ')
+	p array
+	
+	array.each do |f|
+		puts `touch #{f}.md`
+	end
+
 end
 
-make_me_some_markdown
+make_me_some_markdown_multi_ball
 
