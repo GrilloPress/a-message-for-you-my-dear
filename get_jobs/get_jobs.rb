48ddd9495505ebd7103dedb8a86f64a2b2787fca
GrilloPress <grillopress@gmail.com>
2016-05-12 22:45:31 +0000

    add basic url get

--- get_jobs/get_jobs.rb
+++ get_jobs/get_jobs.rb
@@ -1,1 +1,8 @@
-url = "http://www.jobs.nhs.uk/extsearch?client_id=121486&resonly=1&max_result=100"
+require 'rubygems'
+require 'bundler/setup'
+require 'nokogiri'
+require 'open-uri'
+
+JOBS_URL = "http://www.jobs.nhs.uk/extsearch?client_id=121486&resonly=1&max_result=100"
+page = Nokogiri::HTML(open(JOBS_URL))
+puts page.css('title')
