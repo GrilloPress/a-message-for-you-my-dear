1f6a1847b228fc2fdd403c7dad7851257a775b67
GrilloPress <grillopress@gmail.com>
2016-05-13 21:42:38 +0000

    add script to remove outer html and correct links

--- get_jobs/get_jobs.rb
+++ get_jobs/get_jobs.rb
@@ -5,4 +5,15 @@ require 'open-uri'
 
 JOBS_URL = "http://www.jobs.nhs.uk/extsearch?client_id=121486&resonly=1&max_result=100"
 page = Nokogiri::HTML(open(JOBS_URL))
-puts page.css('title')
+
+page = page.css('div.vacancy')
+page.each do |v|
+  
+  # make links absolute
+  v.css('h2 a').each do |x|
+    x["href"] = "http://www.nhs.jobs#{x['href']}"
+  end
+  
+end
+
+puts page
