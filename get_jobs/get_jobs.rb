42346c995b97c13a76b2f2ddb3f22ee7ce96cb11
GrilloPress <grillopress@gmail.com>
2016-05-14 14:33:09 +0000

    remove job basket link

--- get_jobs/get_jobs.rb
+++ get_jobs/get_jobs.rb
@@ -6,14 +6,26 @@ require 'open-uri'
 JOBS_URL = "http://www.jobs.nhs.uk/extsearch?client_id=121486&resonly=1&max_result=100"
 page = Nokogiri::HTML(open(JOBS_URL))
 
+# Grab all of the job vacancies then transform inside the block
 page = page.css('div.vacancy')
 page.each do |v|
   
   # make links absolute
-  v.css('h2 a').each do |x|
+  v.css('a').each do |x|
     x["href"] = "http://www.nhs.jobs#{x['href']}"
   end
   
+  # Remove job basket
+  v.css('.jobBasket').each do |x|
+    x.remove
+  end
+  
+  #remove job basket
+  
+  # Add row class to vacancy summary block
+  
+  # add bootstrap styles within vacancy summary block - left and right col-md-6
 end
 
-File.open("out.html", "a+") {|f| f.write(page) }
+# create or overwrite a file called out.html with the contents of page
+File.open("out.html", "w") {|f| f.write(page) }
